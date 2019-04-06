da05417dd394b91c8c9cf6bd5aa7b9c98c13b780
Ruairidh Holwill <roore@hotmail.co.uk>
2019-04-06 20:09:59 +0100

    customer afford pet tets done

--- pet_shop.rb
+++ pet_shop.rb
@@ -69,3 +69,15 @@ end
 def add_pet_to_customer(customer, new_pet)
   return customer[:pets].push(new_pet)
 end
+
+def customer_can_afford_pet(customer, new_pet)
+  if customer[:cash] > new_pet[:price]
+    return true
+  else
+    return false
+  end
+end
+
+def sell_pet_to_customer(pet_shop, pet, customer)
+  
+end
